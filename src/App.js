5f8ea98b1880186e90b5cfdc0931e7c67edd9cc4
Anand <akishore159@gmail.com>
2022-10-07 20:35:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
